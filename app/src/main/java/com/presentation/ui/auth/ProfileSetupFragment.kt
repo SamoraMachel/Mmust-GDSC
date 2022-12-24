d49454b1799d635f4ca3e98082ed62d72a267226
Samora Machel <samora@me.laptop>
2022-12-24 15:13:22 +0300

    Added gallery image selection from the profile setup fragment

--- app/src/main/java/com/presentation/ui/auth/ProfileSetupFragment.kt
+++ app/src/main/java/com/presentation/ui/auth/ProfileSetupFragment.kt
@@ -1,17 +1,29 @@
 package com.presentation.ui.auth
 
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.activityViewModels
+import com.bumptech.glide.Glide
 import com.presentation.ui.auth.viewmodels.LoginViewModel
 import com.test.mmustgdsc.R
 import com.test.mmustgdsc.databinding.FragmentProfileSetupBinding
 
 
 class ProfileSetupFragment : Fragment() {
+    private val GALLERY_REQUEST = 400
+    private val GALLERY_IMAGE_REQUEST = 40
+
+    private var profileImageToUpload : Uri? = null
 
     private var _binding : FragmentProfileSetupBinding? = null
     private val binding get() = _binding!!
@@ -28,7 +40,66 @@ class ProfileSetupFragment : Fragment() {
     ): View? {
         _binding = FragmentProfileSetupBinding.inflate(inflater, container, false)
 
+        binding.profileImageEdit.setOnClickListener {
+            requestGallery()
+        }
+
         return binding.root
+    }
+
+    private fun requestGallery() {
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                android.Manifest.permission.READ_EXTERNAL_STORAGE) ==
+                PackageManager.PERMISSION_GRANTED
+        ) {
+            openGallery()
+        } else {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(
+                    arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                    GALLERY_REQUEST
+                )
+            }
+        }
+    }
+
+    private fun openGallery() {
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, false)
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), GALLERY_IMAGE_REQUEST)
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == GALLERY_IMAGE_REQUEST && resultCode == Activity.RESULT_OK) {
+            val imageurl  = data?.data
+            imageurl?.let { uri ->
+                profileImageToUpload = uri
+                Glide.with(this)
+                    .load(uri)
+                    .into(binding.profileImage)
+            }
+        }
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode) {
+            GALLERY_REQUEST -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    openGallery()
+                }
+            }
+        }
     }
 
     override fun onDestroy() {
