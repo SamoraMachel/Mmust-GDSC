5ed3f7f10421c228f39ef3af2170017f8f208116
Samora Machel <samora@me.laptop>
2022-12-24 12:15:20 +0300

    Added registration login : usecase and viewmodel registration ui state

--- app/src/main/java/com/presentation/ui/auth/viewmodels/LoginViewModel.kt
+++ app/src/main/java/com/presentation/ui/auth/viewmodels/LoginViewModel.kt
@@ -5,9 +5,11 @@ import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import com.domain.models.LoginDto
 import com.domain.models.ObserverDto
+import com.domain.models.RegistrationDto
 import com.domain.repository.UserDataStore
 import com.domain.usecases.*
 import com.presentation.models.ProfilePresentation
+import com.presentation.models.RegistrationPresentation
 import com.presentation.ui.states.AuthenticationUIState
 import com.presentation.ui.states.ProfileCreatedState
 import com.presentation.ui.states.SingleProfileUIState
@@ -28,6 +30,9 @@ class LoginViewModel @Inject constructor(
     private val _userLoggedIn : MutableStateFlow<AuthenticationUIState> = MutableStateFlow(AuthenticationUIState.StandBy)
     val userLoggedIn get() = _userLoggedIn.asLiveData()
 
+    private val _userRegistered : MutableStateFlow<AuthenticationUIState> = MutableStateFlow(AuthenticationUIState.StandBy)
+    val userRegistered get() = _userRegistered.asLiveData()
+
     private val _profileCreated : MutableStateFlow<ProfileCreatedState> = MutableStateFlow(ProfileCreatedState.StandBy)
     val profileCreatedState get() = _profileCreated.asLiveData()
 
@@ -47,6 +52,27 @@ class LoginViewModel @Inject constructor(
                     } else {
                         saveLoginDataStoreUseCase(false)
                         _userLoggedIn.value = AuthenticationUIState.Success(false)
+                    }
+                }
+            }
+        }
+    }
+
+    fun registerUser(registrationDto: RegistrationDto) = viewModelScope.launch {
+        registrationUseCase(registrationDto).collect { observer ->
+            when(observer) {
+                is ObserverDto.Failure -> {
+                    saveLoginDataStoreUseCase(false)
+                    _userRegistered.value = AuthenticationUIState.Failure(observer.message)
+                }
+                is ObserverDto.Loading -> _userRegistered.value = AuthenticationUIState.Loading
+                is ObserverDto.Success -> {
+                    if(observer.data == true) {
+                        saveLoginDataStoreUseCase(true)
+                        saveProfileUserDataStore(true)
+                    } else {
+                        saveProfileUserDataStore(false)
+                        saveLoginDataStoreUseCase(false)
                     }
                 }
             }
