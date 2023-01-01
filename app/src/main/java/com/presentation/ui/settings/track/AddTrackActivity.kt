ef2506e436fbbd2a0ddd496fc935c57b7b65dcb2
Samora Machel <samora@me.laptop>
2023-01-02 00:16:03 +0300

    Added activity for adding a track

---
+++ app/src/main/java/com/presentation/ui/settings/track/AddTrackActivity.kt
@@ -0,0 +1,12 @@
+package com.presentation.ui.settings.track
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.test.mmustgdsc.R
+
+class AddTrackActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_track)
+    }
+}
